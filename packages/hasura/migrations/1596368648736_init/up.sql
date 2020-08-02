523f71ed116351b1432cf54d84c7c34a97b439fe
show <show75@ezweb.ne.jp>
2020-08-02 20:47:23 +0900

    Initial commit

---
+++ packages/hasura/migrations/1596368648736_init/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public."user" (
+    id integer NOT NULL
+);
+CREATE SEQUENCE public.user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
+ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
