3f2bcd11afa509d3de5e9d6aad996c6f49b93781
show <show75@ezweb.ne.jp>
2020-11-07 20:25:43 +0900

    feat: Spinnerの作成と各コンポーネントで適用

--- packages/web/src/components/Pages/UserDetailPage.tsx
+++ packages/web/src/components/Pages/UserDetailPage.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import RoundedButton from "../Atoms/Buttons/RoundedButton";
 import UserDetailCard from "../Organisms/Cards/UserDetailCard";
 import { useAuthContext } from "../../provider/AuthContextProvider";
+import Spinner from "../Atoms/Indicator/Spinner";
 
 type Params = {
   userId: string;
@@ -36,7 +37,7 @@ const UserDetailPage: FC = () => {
   });
 
   console.log("data.", data?.user);
-  if (!data?.user || loading) return <p>Loading...</p>;
+  if (!data?.user || loading) return <Spinner />;
   if (error) return <p>{error.message}</p>;
 
   const onEditMe = () => {};
